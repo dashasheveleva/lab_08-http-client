a162021b3ad3c1fcd35c600d30106d82e8eb1d64
dashasheveleva <photodoshfy@gmail.com>
2022-05-15 04:23:16 -0700

    Update for quality

--- include/http_client.hpp
+++ include/http_client.hpp
@@ -3,5 +3,6 @@
 #ifndef INCLUDE_HTTP_CLIENT_HPP_
 #define INCLUDE_HTTP_CLIENT_HPP_
 
+auto example() -> void;
 
 #endif // INCLUDE_HTTP_CLIENT_HPP_
--- sources/http_client.cpp
+++ sources/http_client.cpp
@@ -1,3 +1,8 @@
 // Copyright 2022 Shevelyova Darya photodoshfy@gmail.com
 
+#include <stdexcept>
 #include <http_client.hpp>
+
+auto example() -> void {
+  throw std::runtime_error("not implemented");
+}
